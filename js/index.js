c725c47a20140cff47e864d153edc2e06137a314
AymericSandoz <sandoz.aymeric@gmail.com>
2023-07-28 16:33:58 +0200

    suppression ellement inutiles du js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,3 @@
-// const getMoviesCategories = async (number_of_movies) => {
-//   fetch("http://127.0.0.1:8000/api/v1/genres/")
-//     .then((response) => response.json())
-//     .then((data) => {
-//       const genres = data.results;
-//       genres.forEach((genre) => {
-
-//       });
-//     })
-//     .catch((error) => {
-//       console.error("Une erreur s'est produite lors de la requête :", error);
-//     });
-// };
-
 const getMoviesCategories = async () => {
   let url = "http://127.0.0.1:8000/api/v1/genres/";
   let categories = [];
@@ -29,20 +15,7 @@ const getMoviesCategories = async () => {
   return categories;
 };
 
-// const getTopRatedMovie = async (number_of_movies) => {
-//   fetch("http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score")
-//     .then((response) => response.json())
-//     .then((data) => {
-//       const sortedMovies = data.results;
-//       const bestMovie = sortedMovies[0];
-
-//       return bestMovie;
-//     })
-//     .catch((error) => {
-//       console.error("Une erreur s'est produite lors de la requête :", error);
-//     });
-// };
-
+// Fonction qui récupère le film le mieux noté
 const getTopRatedMovie = async (number_of_movies) => {
   url = "http://127.0.0.1:8000/api/v1/titles/?sort_by=-imdb_score";
   data = await fetchPage(url);
@@ -91,49 +64,6 @@ const getTopRatedMovies = async (number_of_movies) => {
   return topRatedMovies;
 };
 
-const getThreeRandomCategories = async () => {
-  const categories = await getMoviesCategories();
-
-  let indices = [...Array(categories.length).keys()]; // Création d'un tableau d'indices de 0 à n-1
-
-  let categoryIndices = [];
-  for (let i = 0; i < 3; i++) {
-    let randomIndex = Math.floor(Math.random() * indices.length); // Sélection d'un indice aléatoire
-    let selectedCategoryIndex = indices[randomIndex]; // Récupération de la catégorie correspondante à l'indice
-    categoryIndices.push(selectedCategoryIndex);
-
-    indices.splice(randomIndex, 1); // Suppression de l'indice sélectionné de la liste des indices disponibles
-  }
-
-  // Récupération des catégories correspondantes aux indices choisis
-  let category1 = categories[categoryIndices[0]];
-  let category2 = categories[categoryIndices[1]];
-  let category3 = categories[categoryIndices[2]];
-
-  return [category1, category2, category3];
-};
-
-// const getMoviesFromOneCategories = async (category) => {
-
-//   let url =
-//     "http://127.0.0.1:8000/api/v1/titles/?genre=Comedy&sort_by=-imdb_score";
-//   let movies = [];
-//   firstPageData = await fetchPage(url);
-
-//   movies = movies.concat(firstPageData.results);
-
-//   let nextPageUrl = firstPageData.next;
-
-//   while (nextPageUrl) {
-//     let nextPageData = await fetchPage(nextPageUrl);
-//     movies = movies.concat(nextPageData.results);
-//     nextPageUrl = nextPageData.next;
-
-//   }
-
-//   return movies;
-// };
-
 const getMoviesFromOneCategories = async (category, number_of_movies) => {
   let movies = [];
   const firstPageUrl = `http://127.0.0.1:8000/api/v1/titles/?genre=${category}&sort_by=-imdb_score`;
@@ -152,62 +82,23 @@ const getMoviesFromOneCategories = async (category, number_of_movies) => {
   return movies;
 };
 
-// getTopRatedMovie()
-//   .then((topMovie) => {
-
-//   })
-//   .catch((error) => {
-//     console.error("Une erreur s'est produite lors de la requête :", error);
-//   });
-
-// getTopRatedMovies(7)
-//   .then((topMovies) => {
-
-//   })
-//   .catch((error) => {
-//     console.error("Une erreur s'est produite lors de la requête :", error);
-//   });
-
-// getMoviesCategories()
-//   .then((categories) => {
-
-//   })
-//   .catch((error) => {
-//     console.error("Une erreur s'est produite lors de la requête :", error);
-//   });
-
-// getThreeRandomCategories()
-//   .then((categories) => {
-
-//   })
-//   .catch((error) => {
-//     console.error("Une erreur s'est produite lors de la requête :", error);
-//   });
-
-// getMoviesFromOneCategories("Comedy", 7)
-//   .then((movies) => {
-
-//   })
-//   .catch((error) => {
-//     console.error("Une erreur s'est produite lors de la requête :", error);
-//   });
-
 const createCategorySection = async (section_id, category_name) => {
   const movies = await getMoviesFromOneCategories(category_name, 7);
   const section = document.getElementById(`${section_id}`);
   const h1 = document.createElement("h1");
   h1.textContent = category_name;
   section.appendChild(h1);
+
   let bloc__images = document.createElement("div");
   let main_category_bloc_content = document.createElement("div");
   bloc__images.classList.add("bloc__images");
   main_category_bloc_content.appendChild(bloc__images);
   main_category_bloc_content.classList.add("main_bloc_content");
   section.appendChild(main_category_bloc_content);
+
   display_images(movies, bloc__images);
-  console.log(category_name, document.querySelector("main").offsetWidth);
+
   if (document.querySelector("main").offsetWidth > 920) {
-    console.log("hahah wtf");
     initSwiper(section_id, movies);
   }
 };
@@ -223,7 +114,6 @@ const display_images = (
   if (document.querySelector("main").offsetWidth <= 920) {
     // If the screen width is less than or equal to 480px (mobile), set selectedMovies to movies
     selectedMovies = movies;
-    console.log("test 920");
   } else {
     // If the screen width is greater than 480px (not mobile), select movies based on the indices
     selectedMovies = movieIndicesToDisplay.map((index) => movies[index]);
@@ -242,8 +132,6 @@ const display_images = (
     });
   });
 };
-
-//il faudrait idéalement fusionner cette focntion avec celle du haut car elle est un peu redondante
 const create_popular_movies_section = async (movies) => {
   const section = document.getElementById("main_category_bloc");
   const h1 = document.createElement("h1");
@@ -281,11 +169,8 @@ getTopRatedMovie()
     title.innerText = movie.title;
     const description = document.querySelector(".main__movie__description");
     description.innerText = movie.description;
-    let button = document.querySelector(".main__movie__button");
-    // button.setAttribute("data-movie", movie);
-    image.addEventListener("click", function () {
-      // data_movie = this.getAttribute("data-movie");
 
+    image.addEventListener("click", function () {
       openModal(movie);
     });
   })
@@ -301,38 +186,11 @@ getTopRatedMovies(7)
     console.error("Une erreur s'est produite lors de la requête :", error);
   });
 
-//le problème de cette fonction c'est que pas pour certaine catégories(celle qui ont moins d'une page ça fait un pug je crois)
-// getThreeRandomCategories()
-//   .then((categories) => {
-
-//     createCategorySection(bloc[1], categories[0].name);
-//     createCategorySection(bloc[2], categories[1].name);
-//     createCategorySection(bloc[3], categories[2].name);
-//   })
-//   .catch((error) => {
-//     console.error("Une erreur s'est produite lors de la requête :", error);
-//   });
-
 const createCategorieSections = async (categories) => {
   createCategorySection(bloc[1], categories[0]);
   createCategorySection(bloc[2], categories[1]);
   createCategorySection(bloc[3], categories[2]);
 };
-
-// const add_modal_events = async () => {
-//   let modal_triggers = document.querySelectorAll(".js-trigger-modal");
-
-//   for (trigger of modal_triggers) {
-
-//     trigger.addEventListener("click", function (event) {
-
-//       data_movie = this.getAttribute("data-movie");
-//       let title = data_movie.title;
-//       let description = data_movie.description;
-//       openModal(title, description);
-//     });
-//   }
-// };
 
 const createWrapper = (elementType, value, labelText, isList = false) => {
   let wrapper = document.createElement("div");
@@ -388,12 +246,6 @@ const openModal = async (movie) => {
 
   modal_left_content.appendChild(date_and_rating_container);
 
-  // let modalDescription = document.createElement("p");
-  // modalDescription.innerText = movie.description;
-  // let modalTitleWrapper = document.createElement("div"); // Élément conteneur pour le préfixe
-  // modalTitleWrapper.className = "label"; // Ajout de la classe "label" à l'élément conteneur
-  // modalTitleWrapper.innerText = "Titre : "; // Préfixe "Titre :"
-
   modalDescription = createWrapper("p", movie.description, "Description");
   modal_left_content.appendChild(modalDescription);
 
@@ -431,16 +283,6 @@ const openModal = async (movie) => {
     modal_right_content.appendChild(modalLongDescription);
   }
 
-  // let modalActors = document.createElement("ul");
-
-  // for (let actor of movie.actors) {
-  //   let li = document.createElement("li");
-
-  //   li.innerText = actor;
-  //   modalActors.appendChild(li);
-  // }
-  // modal_right_content.appendChild(modalActors);
-
   if (movie.actors) {
     modalActors = createWrapper("div", movie.actors, "Acteurs", true);
     modal_right_content.appendChild(modalActors);
@@ -453,8 +295,7 @@ const openModal = async (movie) => {
 
 const closeModal = () => {
   let modal = document.getElementById("myModal");
-
-  modal.style.display = "none"; // Cache la modal
+  modal.style.display = "none";
   let modal_content = document.querySelector(".modal-content");
   modal_content.innerHTML = "";
   document.querySelector("main").style.opacity = 1;
@@ -485,6 +326,7 @@ function initSwiper(containerId, movies) {
   // Fonction pour afficher l'image suivante
   function showNextImage() {
     currentStartIndex = currentStartIndex + 1;
+
     display_images(movies, bloc_images, [
       currentStartIndex % 7,
       (currentStartIndex + 1) % 7,
@@ -499,12 +341,7 @@ function initSwiper(containerId, movies) {
     if (currentEndIndex == -5) {
       currentEndIndex = 2;
     }
-    console.log("maide", currentEndIndex, " :", [
-      (currentEndIndex - 3 + 7) % 7,
-      (currentEndIndex - 2 + 7) % 7,
-      (currentEndIndex - 1 + 7) % 7,
-      (currentEndIndex + 7) % 7,
-    ]);
+
     display_images(movies, bloc_images, [
       (currentEndIndex - 3 + 7) % 7,
       (currentEndIndex - 2 + 7) % 7,
@@ -525,13 +362,12 @@ close_modal.addEventListener("click", function () {
   closeModal();
 });
 
-// Sélectionnez la modal
 let modal = document.getElementById("myModal");
 
 // Ajoutez un gestionnaire d'événements au clic sur le body
 document.body.addEventListener("click", function (event) {
   // Vérifiez si l'élément cliqué est en dehors de la modal
   if (event.target != modal) {
-    closeModal(); // Masquer la modal
+    closeModal();
   }
 });
